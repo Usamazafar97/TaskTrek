e574fd8c6b7247903068cdb5a0deb7494ae88075
Usama Zafar <osamazafar97gmail.com>
2024-08-17 17:54:18 +0200

    first commit

---
+++ src/CreateTodo.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react"; // useState -> Hook (functions to help converting functional components
+// to class components)
+
+function CreateTodo(params) {
+  const [count, setCount] = useState(1); // 0, default value
+  const [newTitle, setTitle] = useState("Write title here");
+
+  //   function checkIfEmpty() {
+  //     return count != 0 ? `Tasks : ${count}` : "No tasks available";
+  //   }
+
+  const onClickButton = () => {
+    setCount(count + 1);
+    checkIfEmpty();
+    console.log("added");
+  };
+
+  const handleTextChanged = (event) => {
+    console.log(event.target.value);
+
+    setTitle(event.target.value);
+  };
+
+  //   function setDisability() {
+  //     return count != 0 ? false : true;
+  //   }
+
+  return (
+    <React.Fragment>
+      <h1>Title : {newTitle.length == 0 ? "Write something" : newTitle}</h1>
+      <h2>Tasks : {count}</h2>
+      <input onChange={handleTextChanged}></input>
+      {/* <h1>{checkIfEmpty()}</h1> */}
+      {/* <button onClick={onClickButton} disabled={setDisability()}> */}
+      <button onClick={onClickButton}>Add me</button>
+    </React.Fragment>
+  );
+}
+
+export default CreateTodo;
