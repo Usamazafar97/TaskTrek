e714e3c7a9149f345a9223a3db8cb2c588bd4f21
Usama Zafar <osamazafar97gmail.com>
2024-08-23 19:52:23 +0200

    Adding task feature - done

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./Tag.css";
 
-const Tag = (props) => {
-  return <button className="tag">{props.tagName}</button>;
+const Tag = ({ tagName, handleTags, selected }) => {
+  const tagStyle = {
+    HTML: { backgroundColor: "#fda821" },
+    CSS: { backgroundColor: "#15d4c8" },
+    Javascript: { backgroundColor: "#ffd12c" },
+    React: { backgroundColor: "#4adafc" },
+    Default: { backgroundColor: "#f9f9f9" },
+  };
+  return (
+    <button
+      style={selected ? tagStyle[tagName] : tagStyle.Default}
+      type="button"
+      className="tag"
+      onClick={() => {
+        handleTags(tagName);
+      }}
+    >
+      {tagName}
+    </button>
+  );
 };
 
 export default Tag;
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,27 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TaskForm.css";
 import Tag from "./Tag";
 
 const TaskForm = () => {
+  // const [taskData, setTask] = useState("");
+  // const [tagData, setTagData] = useState("");
+  // const [taskStatusData, setTaskStatusData] = useState("");
+
+  const [taskData, setTaskData] = useState({
+    name: "",
+    status: "todo",
+    tags: [],
+  });
+
+  const handleTaskData = (event) => {
+    const { name, value } = event.target;
+
+    setTaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleTags = (tag) => {
+    // logic for checking if the tag is already in the array or not
+    if (taskData.tags.some((item) => tag === item)) {
+      const filteredTags = taskData.tags.filter((item) => tag !== item);
+      setTaskData((prev) => {
+        return { ...prev, tags: filteredTags };
+      });
+    } else {
+      setTaskData((prev) => {
+        return { ...prev, tags: [...prev.tags, tag] };
+      });
+    }
+  };
+
+  console.log(taskData.tags);
+
+  const selectedTags = (tag) => {
+    return taskData.tags.some((item) => tag === item);
+  };
+
+  // const handleTaskStatusData = (event) => {
+  //   setTaskStatusData(event.target.value);
+  // };
+
+  const handleSubmitDData = (event) => {
+    event.preventDefault();
+    console.log(taskData);
+  };
+
   return (
     <header className="app_header">
-      <form>
+      <form onSubmit={handleSubmitDData}>
         <input
           type="text"
+          name="name"
           className="task_input"
           placeholder="Enter your task"
+          onChange={handleTaskData}
         />
 
         <div className="task_form_bottom_line">
           <div>
-            <Tag tagName="HTML" />
-            <Tag tagName="CSS" />
-            <Tag tagName="Javascript" />
-            <Tag tagName="React" />
+            <Tag
+              tagName="HTML"
+              handleTags={handleTags}
+              selected={selectedTags("HTML")}
+            />
+            <Tag
+              tagName="CSS"
+              handleTags={handleTags}
+              selected={selectedTags("CSS")}
+            />
+            <Tag
+              tagName="Javascript"
+              handleTags={handleTags}
+              selected={selectedTags("Javascript")}
+            />
+            <Tag
+              tagName="React"
+              handleTags={handleTags}
+              selected={selectedTags("React")}
+            />
           </div>
 
           <div>
-            <select className="task_status">
+            <select
+              name="status"
+              className="task_status"
+              onChange={handleTaskData}
+            >
               <option value="todo">To do</option>
               <option value="doing">Doing</option>
               <option value="done">Done</option>
